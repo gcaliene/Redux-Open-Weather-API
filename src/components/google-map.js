cc29922b15ef6afca6b0db508048517fce1517e3
Windows <gersoncalienes@gmail.com>
2018-03-10 16:42:07 -0500

    added google-map.js component

---
+++ src/components/google-map.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class GoogleMap extends Component {
+  componentDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon
+      }
+    });
+  }
+  render() {
+    //this.ref.map will get me a direct reference to this map
+    return <div ref="map" />;
+  }
+}
+
+export default GoogleMap;
