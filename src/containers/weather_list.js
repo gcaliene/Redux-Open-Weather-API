f805daccc4c24f402ccfe6775e242765a59ec434
Windows <gersoncalienes@gmail.com>
2018-03-09 22:20:46 -0500

    Started weather_list container

---
+++ src/containers/weather_list.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class WeatherList extends Component {
+  render() {
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th>City</th>
+        </tr>
+      </thead>
+      <tbody />
+    </table>;
+  }
+}
